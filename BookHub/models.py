91b778f5359fb77fc6d48412ae14c097f914c9c1
Artur <arturilamonta@gmail.com>
2024-11-08 21:16:14 +0200

    First start

--- BookHub/models.py
+++ BookHub/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+
+    path = models.FileField(upload_to='media/books/')
+
+    class Meta:
+
+        def __str__(self):
+            return self.path
--- BookHub/views.py
+++ BookHub/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from .models import Book
+from .forms import BookForm
 # Create your views here.
+
+
+def main(request):
+
+    form = BookForm()
+
+    if request.method == "POST":
+
+        form = BookForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+    else:
+        form = BookForm()
+
+    return render(request, "BookHub/main.html", context={'form':form})
