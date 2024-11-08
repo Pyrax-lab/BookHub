cc171d870785e808c6fdeeccb3844fcf9714b0cf
Artur <arturilamonta@gmail.com>
2024-11-08 21:26:14 +0200

    Added users

--- BookHub/models.py
+++ BookHub/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 
 class Book(models.Model):
 
-    path = models.FileField(upload_to='media/books/')
+    path = models.FileField(upload_to='books/', blank=True, null=True)
 
     class Meta:
 
         def __str__(self):
             return self.path
+        
+
--- main/settings.py
+++ main/settings.py
@@ -127,8 +127,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = 'static' 
+STATIC_URL = 'static/' 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-MEDIA_URL = 'media'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = [BASE_DIR / 'media']
