2a29732630378c022c1fc2e875b88cc2970a0b03
Artur <arturilamonta@gmail.com>
2024-11-09 14:19:53 +0200

    You can add the book and the web site displays selected books

--- BookHub/views.py
+++ BookHub/views.py
@@ -1,26 +1,23 @@
 from django.shortcuts import render
 from .models import Book
 from .forms import BookForm
+from users.models import User
 # Create your views here.
 
 
 def main(request):
 
-    
+    get_user = User.objects.all().filter(id = request.user.id)
+    user_books = get_user[0].books_read.all()
 
     form = BookForm()
-
     if request.method == "POST":
-
         form = BookForm(request.POST, request.FILES)
-
         if form.is_valid():
-            form.save()
-            #print(book)
-            #user = request.user
-            #   print(user)
-            #user.books_read.add(book)  
+            book = form.save()
+            user = request.user
+            user.books_read.add(book)  
     else:
         form = BookForm()
 
-    return render(request, "BookHub/main.html", context={'form':form})
+    return render(request, "BookHub/main.html", context={'form':form, "user_books" : user_books})
